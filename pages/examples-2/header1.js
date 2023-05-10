80e621ee4090010d26ef683b1b218dbc67ceda1f
InjuMichorius <inju@hotmail.nl>
2023-05-10 16:10:10 +0200

    changed map structure and new header v2

---
+++ pages/examples-2/header1.js
@@ -0,0 +1,44 @@
+// Appear on scroll
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+// Move navbar on scroll
+document.getElementById("hbNavbar").style.transform = "translateY(-400%)";
+
+window.onscroll = function () { scrollFunction(); };
+
+function scrollFunction() {
+    console.log(document.documentElement.scrollTop)
+    console.log(window.innerHeight)
+    const breakpoint = window.innerHeight - 140
+    if (document.body.scrollTop > breakpoint || document.documentElement.scrollTop > breakpoint) {
+        document.getElementById("fullNavbar").style.transform = "translateY(-100%)";
+        document.getElementById("hbNavbar").style.transform = "translateY(0)";
+    } else {
+        document.getElementById("hbNavbar").style.transform = "translateY(-100%)";
+        document.getElementById("fullNavbar").style.transform = "translateY(0)";
+    }
+}
+
+// Blob follow
+const blob=document.querySelector('.blob');
+window.addEventListener('pointermove',(e)=>{
+  const {clientX,clientY}=e;
+  console.log(clientX,clientY)
+  // blob.style.left=`${clientX}px`;
+  // blob.style.top=`${clientY}px`;
+  blob.animate({
+    left:`${clientX}px`,
+    top:`${clientY}px`
+  },{duration:3000, fill:"forwards"})
+})
